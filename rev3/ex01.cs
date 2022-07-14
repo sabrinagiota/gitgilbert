f2df8baea0e1e4ffec9ff104f9a4ad284b061293
sabrinagiota <79128442+sabrinagiota@users.noreply.github.com>
2022-07-13 21:22:36 -0300

    Create ex01.cs

---
+++ rev3/ex01.cs
@@ -0,0 +1,12 @@
+using System; 
+
+class Program {
+	public static void Main (string[] args) {
+		int a = 1;
+		Console.WriteLine("ordem crescente");
+		while (a <= 10) {
+			Console.Write(a);
+			a++;
+		}	
+	}
+}
