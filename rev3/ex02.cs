1d587a1772bddda9b55a1df1c41e635240a82c0e
sabrinagiota <79128442+sabrinagiota@users.noreply.github.com>
2022-07-13 21:23:26 -0300

    Create ex02.cs

---
+++ rev3/ex02.cs
@@ -0,0 +1,13 @@
+using System; 
+
+class Program {
+	public static void Main (string[] args) {
+		
+		int a = 10;
+		Console.WriteLine("ordem decrescente");
+		do {
+			Console.Write(a);
+			a--;
+		}	while (a >= 1);
+	}
+}
