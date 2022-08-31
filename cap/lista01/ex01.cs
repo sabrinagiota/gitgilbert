d7afc5b769d930f9e5776722e0343c22cc6dcb49
sabrinagiota <79128442+sabrinagiota@users.noreply.github.com>
2022-08-31 14:21:34 -0300

    Create ex01.cs

---
+++ cap/lista01/ex01.cs
@@ -0,0 +1,12 @@
+using system;
+
+class Program {
+	public static void main () {
+
+		Console.WriteLine("digite seu nome:");
+
+		string nome = Console.ReadLine();
+
+		Console.WriteLine($"Olá, {nome}, tudo bem!");
+	}
+}
